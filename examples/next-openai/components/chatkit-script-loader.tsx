1d058d5ea72347d64008cfd373e59b77fea6b9e1
Renol1 <tokarp211@gmail.com>
2025-11-06 18:03:33 +0000

    next-openai: add console log when ChatKit SDK script loads to verify readiness

--- examples/next-openai/components/chatkit-script-loader.tsx
+++ examples/next-openai/components/chatkit-script-loader.tsx
@@ -9,6 +9,12 @@ export default function ChatKitScriptLoader() {
       strategy="afterInteractive"
       onLoad={() => {
         if (typeof window !== 'undefined') {
+          // Simple debug log to verify the SDK script has loaded in production
+          // You can remove this after verifying things work.
+          // eslint-disable-next-line no-console
+          console.log('[ChatKit] SDK script loaded', {
+            hasGlobal: Boolean((window as any).OpenAIChatKit),
+          });
           window.dispatchEvent(new Event('chatkit:loaded'));
         }
       }}
